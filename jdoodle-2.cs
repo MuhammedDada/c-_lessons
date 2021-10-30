3473d6d6f5a0491efd11eb7e9fee490b64e8ebf7
MuhammedDada <93438818+MuhammedDada@users.noreply.github.com>
2021-10-30 19:28:10 +0500

    Add files via upload

---
+++ jdoodle-2.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args) {
+       
+       #region login
+       string UserName; 
+       string PassWord; 
+       //output
+       Console.WriteLine(write your UserName);
+       //input
+       UserName = Console.ReadLine();
+       //output 
+       Console.WriteLine = (write your PassWord)
+       //input 
+       PassWord= Console.ReadLine();
+        
+       #region and (logical operator)
+       if (UserName =="Ahmed" || UserName =="Mohamed" || UserName == "Reham" || UserName == "Mona")
+ 
+       if (PassWord=="hoopoe2021")
+      
+      Console.WriteLine("Welcome")
+
+      
+     else 
+
+     Console.WriteLine("username or password not correct")
+    
+        
+        else
+        {
+            Console.WriteLine("password not correct")
+        }
+
+
+    }
+}
