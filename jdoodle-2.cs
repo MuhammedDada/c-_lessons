89e6ff0ddade59ec26a935cf792206d02fbc8970
MuhammedDada <93438818+MuhammedDada@users.noreply.github.com>
2021-10-31 19:43:10 +0500

    Update jdoodle-2.cs

--- jdoodle-2.cs
+++ jdoodle-2.cs
@@ -12,28 +12,25 @@ class Program
        //input
        UserName = Console.ReadLine();
        //output 
-       Console.WriteLine = ("write your PassWord")
+       Console.WriteLine = ("write your PassWord");
        //input 
        PassWord= Console.ReadLine();
         
        #region and (logical operator)
        if (UserName =="Ahmed" || UserName =="Mohamed" || UserName == "Reham" || UserName == "Mona")
- 
-       if (PassWord=="hoopoe2021")
-      
-      Console.WriteLine("Welcome");
-
-      
-     else 
-
-     Console.WriteLine("username or password not correct");
-    
-        
+    {
+           
+           if (PassWord=="hoopoe2021");
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine.("password not correct");
+            }
         else
         {
-            Console.WriteLine("password not correct");
+            Console.WriteLine("UserName or password not correct");
         }
 
-
     }
-}
