4c6f88c38ee57d9a3ae72a72194c632ee6432a04
MuhammedDada <93438818+MuhammedDada@users.noreply.github.com>
2021-11-13 21:33:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Account
+{
+    class User
+    {
+        public string FirstName;
+        public string lastName;
+        public string UserName;
+        public string Email;
+        public DateTime BirthDay;
+        public double monthlysalary;
+        public string mobile;
+        public double nationalid;
+        private string number;
+        public string Mobile()
+        {
+            return mobile;
+        }
+        private string Name;
+        public string FullName()
+        {
+            Name = string.Concat(FirstName + lastName);
+            return Name;
+        }
+        private DateTime today = DateTime.Now;
+        private int Age;
+        public int CalAge()
+        {
+            Age = today.Year - BirthDay.Year;
+            return Age;
+        }
+    }
+}
